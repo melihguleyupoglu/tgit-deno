7d847de6821d4515d25621b10d7ef73503e4fd32
melihguleyupoglu <melihguleyup@gmail.com>
2025-01-14 17:26:41 +0300

    add logging the root content

--- src/commands/status.ts
+++ src/commands/status.ts
@@ -1,32 +1,58 @@
+import { inflate } from "https://deno.land/x/compress@v0.5.5/mod.ts";
+
+const currentBranchName = (await Deno.readTextFile(".tgit/HEAD"))
+  .split("/")[2]
+  .trim();
 export default async function status() {
-  const currentBranchName = (await Deno.readTextFile(".tgit/HEAD")).split(
-    "/"
-  )[2];
   console.log(`On branch ${currentBranchName}`);
   let ignoreContent = [] as string[];
-  if (await Deno.lstat(".tgitignore")) {
-    ignoreContent = (await Deno.readTextFile(".tgitignore")).split("\n");
-  }
-  for await (const file of Deno.readDir("")) {
-    if (file.name in ignoreContent) {
-      continue;
-    } else {
-      await checkForToBeCommitted(file.name);
+  try {
+    await checkForToBeCommitted("baban");
+    for await (const file of Deno.readDir("")) {
+      if (file.name in ignoreContent) {
+        continue;
+      } else {
+        // await checkForToBeCommitted(file.name);
+      }
     }
+  } catch (error) {
+    console.error(error);
   }
 }
 
 async function checkForToBeCommitted(fileName: string): Promise<void> {
-  const indexContent = await Deno.readTextFile(".tgit/index");
-  const mtime = (await Deno.lstat(fileName)).mtime ?? 0;
-  const lines = indexContent.split("\n");
-  console.log("Changes to be committed:\n");
-  for (const entry of lines) {
-    if (entry[2] === fileName && entry[3] !== mtime.toString()) {
-      console.log(`\tmodified: ${fileName}\n`);
-    }
-    // else if()
-  }
+  // const indexContent = await Deno.readTextFile(".tgit/index");
+  // const mtime = (await Deno.lstat(fileName)).mtime ?? 0;
+  // const lines = indexContent.split("\n");
+  // console.log("Changes to be committed:\n");
+  // for (const entry of lines) {
+  //   if (entry[2] === fileName && entry[3] !== mtime.toString()) {
+  //     console.log(`\tmodified: ${fileName}\n`);
+  //   }
+  //   // else if()
+  // }
+  const commitHash = await Deno.readTextFile(
+    `.tgit/refs/heads/${currentBranchName}`
+  );
+  const commitObjectCompressed = await Deno.readFile(
+    `.tgit/objects/${
+      commitHash.charAt(0) + commitHash.charAt(1)
+    }/${commitHash.slice(2)}`
+  );
+  const uint8Array = inflate(commitObjectCompressed);
+  const decodedCommitObject = new TextDecoder().decode(uint8Array);
+  const rootHash = decodedCommitObject.split("\n")[0].split(" ")[1];
+  const rootHashContent = await Deno.readTextFile(
+    `.tgit/objects/${rootHash.charAt(0) + rootHash.charAt(1)}/${rootHash.slice(
+      2
+    )}`
+  );
+  const rootContentHash = rootHashContent.split("\0")[1];
+  const rootContentHashContent = await Deno.readTextFile(
+    `.tgit/objects/${
+      rootContentHash.charAt(0) + rootContentHash.charAt(1)
+    }/${rootContentHash.slice(2)}`
+  );
 }
 
 async function checkForNotStaged(fileName: string): Promise<void> {}
